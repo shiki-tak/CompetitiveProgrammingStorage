1f275429625820a2e0524715c02a1970dbfa7d18
shiki-tak <shiki.tak@gmail.com>
2017-10-24 20:05:22 +0900

    eratosthenes algorithm complete

--- chapter11/check_square_over_target_number.rb
+++
@@ -1,9 +0,0 @@
-class Checksquareovertargetnumber
-  def check_square_over_target_number(search_range, target_array)
-    if search_range * search_range <= target_array.length
-      return true
-    else
-      return false
-    end
-  end
-end
--- chapter11/init_array.rb
+++
@@ -1,8 +0,0 @@
-class Initarray
-  def init_array(array_range, target_array)
-    for i in 0..array_range
-      target_array[i] = 1
-    end
-    return target_array
-  end
-end
