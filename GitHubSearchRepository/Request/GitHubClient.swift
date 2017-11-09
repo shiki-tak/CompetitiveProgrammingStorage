d75df96a35f6285f4ef3794167a8bb5199c5d523
shiki-tak <shiki.tak@gmail.com>
2017-11-09 10:00:45 +0900

    add urlRequest

--- GitHubSearchRepository/Request/GitHubClient.swift
+++ GitHubSearchRepository/Request/GitHubClient.swift
@@ -17,7 +17,28 @@ class GitHubClient {
     
     func send<Request: GitHubRequest>(
         request: Request,
-        completion: (Result<Request.Response, GitHubClientError>) ->
+        completion: @escaping (Result<Request.Response, GitHubClientError>) ->
         Void) {
+        let urlRequest = request.buildURLRequest()
+        let task = session.dataTask(with: urlRequest) {
+            data, response, error in
+            
+            switch (data, response, error) {
+            case (_, _, let error?):
+                completion(Result(error: .connectionError(error)))
+            case (let data?, let response?, _):
+                do {
+                    let response = try request.response(from: data, urlResponse: response)
+                    completion(Result(value: response))
+                } catch let error as GitHubAPIError {
+                    completion(Result(error: .apiError(error)))
+                } catch {
+                    completion(Result(error: .responseParseError(error)))
+                }
+            default:
+                fatalError("invalid response combination \(data), \(response), \(error).")
+            }
+        }
+        task.resume()
     }
 }
