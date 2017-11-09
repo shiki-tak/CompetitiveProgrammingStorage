20e58f5658429d2855dd4355e2a7524ce7d09a41
shiki-tak <shiki.tak@gmail.com>
2017-11-09 09:27:17 +0900

    add SearchUsers struct

--- GitHubSearchRepository/Request/GitHubAPI.swift
+++ GitHubSearchRepository/Request/GitHubAPI.swift
@@ -26,4 +26,22 @@ final class GitHubAPI {
             return ["q": keyword]
         }
     }
+    
+    struct SearchUsers: GitHubRequest {
+        let keyword: String
+        
+        typealias Response = SearchResponse<User>
+        
+        var method: HTTPMethod {
+            return .get
+        }
+        
+        var path: String {
+            return "/search/users"
+        }
+        
+        var parameters: Any? {
+            return ["q": keyword]
+        }
+    }
 }
