7cbeb0371a899f4a1871c245ec1207766b4be019
shiki-tak <shiki.tak@gmail.com>
2017-11-16 10:43:28 +0900

    add Crowdsale.json

--- ico-contract/migrations/2_deploy_contracts.js
+++ ico-contract/migrations/2_deploy_contracts.js
@@ -3,3 +3,4 @@ const fs = require('fs');
 const TakFund = artifacts.require('TakFund.sol');
 const TakCrowdsale = artifacts.require('TakCrowdsale.sol');
 const fundParams = JSON.parse(fs.readFileSync('../config/TakFund.json', 'utf8'));
+const crowdsaleParams = JSON.parse(fs.readFileSync('../config/Crowdsale.json', 'utf8'));
