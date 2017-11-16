c8164b8cfc5f8926c41a41523171063c093e5d18
shiki-tak <shiki.tak@gmail.com>
2017-11-16 11:36:26 +0900

    fix truffle.js

--- ico-contract/truffle.js
+++ ico-contract/truffle.js
@@ -1,4 +1,25 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*", // Match test network id
+      gas: 4400000,
+      gasPrice: 21000000000
+    },
+    testnet: {
+      host: "192.168.0.103",
+      port: 8545,
+      network_id: 3, // Match ropsten network id
+      gas: 3500000,
+      gasPrice: 21000000000
+    },
+    mainnet: {
+      host: "10.23.122.2",
+      port: 8545,
+      network_id: 1, // Match main network id
+      gas: 4400000,  // Gaslimit based on latestblock
+      gasPrice: 21000000000
+    }
+  }
 };
