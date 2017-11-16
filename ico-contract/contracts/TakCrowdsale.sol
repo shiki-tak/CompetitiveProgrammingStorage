cc9b135d3c11dadf422cea17ab5a3b94f4330e22
shiki-tak <shiki.tak@gmail.com>
2017-11-16 11:12:34 +0900

    add WhitelistedCrowdsale.sol

--- ico-contract/contracts/TakCrowdsale.sol
+++ ico-contract/contracts/TakCrowdsale.sol
@@ -4,10 +4,11 @@ import 'zeppelin-solidity/contracts/crowdsale/CappedCrowdsale.sol';
 import 'zeppelin-solidity/contracts/crowdsale/RefundableCrowdsale.sol';
 import 'zeppelin-solidity/contracts/token/MintableToken.sol';
 import 'zeppelin-solidity/contracts/lifecycle/Pausable.sol';
+import './WhitelistedCrowdsale.sol';
 import './TakToken.sol';
 
 
-contract TakCrowdsale is CappedCrowdsale, RefundableCrowdsale, Pausable {
+contract TakCrowdsale is CappedCrowdsale, RefundableCrowdsale, Pausable, WhitelistedCrowdsale {
 
   uint256 constant RATE_PRE_SALE = 20000;
   uint256 constant RATE_WEEK_1 = 2900;
