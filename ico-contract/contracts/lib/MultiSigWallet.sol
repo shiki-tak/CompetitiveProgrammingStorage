2e8efed78d06061ca0c0fd43874752ba9ee7d1ff
user1 <user1@user1noMacBook-Pro.local>
2017-11-16 15:38:41 +0900

    add private.genesis.json

--- ico-contract/contracts/lib/MultiSigWallet.sol
+++ ico-contract/contracts/lib/MultiSigWallet.sol
@@ -107,9 +107,8 @@ contract MultiSigWallet {
         validRequirement(_owners.length, _required)
     {
         for (uint i=0; i<_owners.length; i++) {
-            if (isOwner[_owners[i]] || _owners[i] == 0)
-                throw;
-            isOwner[_owners[i]] = true;
+          require(isOwner[_owners[i]] || _owners[i] == 0);
+          isOwner[_owners[i]] = true;
         }
         owners = _owners;
         required = _required;
