aeb07415685c38d1142047f6f932ff925cbf644e
user1 <user1@user1noMacBook-Pro.local>
2017-11-22 12:48:24 +0900

    fix contract.js

--- web3/test/contract.js
+++ web3/test/contract.js
@@ -3,21 +3,38 @@ var web3 = new Web3();
 
 web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 
-var address = '0x7262f4e71a4385078166e0f5df02fd562d588fc8';
+var address = '0xfc980435077a6b72b9328faf9c0a1fb49f21c2fc';
 
 var abi = [{"constant": true,"inputs": [],"name": "say","outputs": [{"name": "","type": "string"}],"payable": false,"stateMutability": "view","type": "function"},{"constant": false,"inputs": [{"name": "_greeting","type": "string"}],"name": "setGreeting","outputs": [{"name": "","type": "string"}],"payable": false,"stateMutability": "nonpayable","type": "function"},{"constant": true,"inputs": [],"name": "greeting","outputs": [{"name": "","type": "string"}],"payable": false,"stateMutability": "view","type": "function"},{"inputs": [{"name": "_greeting","type": "string"}],"payable": false,"stateMutability": "nonpayable","type": "constructor"}];
 
 var contract = web3.eth.contract(abi).at(address);
 
-// 初期値でHelloを実行
-var response = contract.say.call();
-console.log("Initial value");
-console.log("response: ", response);
+let promise = new Promise((resolve, reject) => {
+  var response = contract.say.call();
+  console.log("Initial value");
+  console.log(response);
+  resolve();
+})
 
-// setGreetで値を変更してHelloを実行
-var greeting = 'Hello, Ethereum!';
-var setGreeting = contract.setGreeting.call(greeting);
+promise.then(() => { // #2
+  return new Promise((resolve, reject) => {
+    var greeting = 'Hello, Ethereum!';
 
-var ether = contract.say.call();
-console.log("Second value");
-console.log("response: ", ether);
+    var result = contract.setGreeting(greeting, {from: web3.eth.accounts[0]}, function(err, res) {
+      if (err) console.log(err);
+      else {
+        console.log("set result");
+        console.log("response: ", res);
+
+      }
+    });
+    resolve();
+    return result;
+  })
+}).then(() => { // #3
+  var response = contract.say.call();
+  console.log("Second value");
+  console.log(response);
+}).catch(() => { // エラーハンドリング
+  console.error('Something wrong!')
+})
