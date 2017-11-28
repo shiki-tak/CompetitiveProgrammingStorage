979fe9e6a83967e7a138fc9a06777c62693254cd
user1 <user1@user1noMacBook-Pro.local>
2017-11-29 08:54:02 +0900

    fix babel

--- truffle.js
+++ truffle.js
@@ -1,11 +1,12 @@
+require('babel-register');
+require('babel-polyfill');
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
   networks: {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*", // Match test network id
+      network_id: "*",
       gas: 4400000,
       gasPrice: 21000000000
     }
