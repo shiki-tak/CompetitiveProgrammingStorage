3baf36493a3ee31d8933dd536686a82a37202610
user1 <user1@user1noMacBook-Pro.local>
2017-11-28 14:46:12 +0900

    add test to Crowdsale

--- test/crowdsale.js
+++ test/crowdsale.js
@@ -1,14 +1,23 @@
+const RuliToken = artifacts.require('RuliToken.sol');
 const Crowdsale = artifacts.require('Crowdsale.sol');
 
-contract('Crowdsale', (accounts) => {
+contract('Crowdsale', () => {
   let crowdSale;
 
   describe('CONTRACT DEPLOYMENT', () => {
-    it('should FIX', () => Crowdsale.deployed().then(
+    it('should has deployed address of RuliToken', () => Crowdsale.deployed().then(
       instance => {
         crowdSale = instance;
-        console.log('pending.');
-        return true;
+        return crowdSale.token().then(
+          tokenAddress => {
+            assert.equal(RuliToken.address, tokenAddress, `wrong token address: ${tokenAddress}`);
+          }
+        )
+      }
+    ));
+    it('should has specified address of RuliToken', () => crowdSale.fund().then(
+      fund => {
+        assert.equal(fund, '0x0000000000000000000000000000000000000000', `wrong token address: ${fund}`);
       }
     ));
   });
