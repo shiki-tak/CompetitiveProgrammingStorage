491b7f04323f1d62c3d420b0b2b9f72e44ff9021
user1 <user1@user1noMacBook-Pro.local>
2017-11-28 15:51:57 +0900

    extends Ownable

--- contracts/RuliToken.sol
+++ contracts/RuliToken.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.4.15;
 
 import "tokens/HumanStandardToken.sol";
+import "zeppelin/contracts/ownership/Ownable.sol";
 
-contract RuliToken is HumanStandardToken {
+contract RuliToken is HumanStandardToken, Ownable {
 
   function RuliToken(
     uint256 _initialAmount,
