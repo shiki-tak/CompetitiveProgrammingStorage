700677912deec409e5965cc954732a9d7527c9c7
user1 <user1@user1noMacBook-Pro.local>
2017-11-28 14:12:31 +0900

    add test of offered amount

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,6 +6,6 @@ const initParams = JSON.parse(fs.readFileSync('../config/RuliToken.json', 'utf8'
 module.exports = function deployContracts(deployer) {
   deployer.deploy(
     RuliToken, initParams.initialAmount, initParams.name, initParams.decimal,
-    initParams.symbol, initParams.offeredAmount,
+    initParams.symbol, initParams.offeredAmount
   );
 };
