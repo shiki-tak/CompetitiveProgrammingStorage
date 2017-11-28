bb0959834d1c102c9fc990fd54fb97b9893e425b
user1 <user1@user1noMacBook-Pro.local>
2017-11-28 12:49:46 +0900

    add offered amount of token

--- contracts/RuliToken.sol
+++ contracts/RuliToken.sol
@@ -3,13 +3,16 @@ pragma solidity ^0.4.15;
 import "tokens/HumanStandardToken.sol";
 
 contract RuliToken is HumanStandardToken {
+  uint256 public offeredAmount;
+  
   function RuliToken(
     uint256 _initialAmount,
     string _tokenName,
     uint8 _decimal,
-    string _tokenSymbol
+    string _tokenSymbol,
+    uint256 _offeredAmount
     )
     HumanStandardToken(_initialAmount, _tokenName, _decimal, _tokenSymbol) {
-      
+      offeredAmount = _offeredAmount;
     }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,6 +2,6 @@ const fs = require('fs');
 const RuliToken = artifacts.require('RuliToken.sol');
 const initParams = JSON.parse(fs.readFileSync('../config/RuliToken.json', 'utf8'));
 
-module.exports = function(deployer) {
-  deployer.deploy(RuliToken, initParams.amount, initParams.name, initParams.decimal, initParams.symbol);
+module.exports = function deployContracts(deployer) {
+  deployer.deploy(RuliToken, initParams.initialAmount, initParams.name, initParams.decimal, initParams.symbol, initParams.offeredAmount);
 };
