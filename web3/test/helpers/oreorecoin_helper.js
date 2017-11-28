d9688a7f234ac79b81670066bc1155c44f0ee107
user1 <user1@user1noMacBook-Pro.local>
2017-11-28 09:14:32 +0900

    add scripts

---
+++ web3/test/helpers/oreorecoin_helper.js
@@ -0,0 +1,3 @@
+const fs = require('fs');
+
+export const OreOreCoin = artifacts.require('OreOreCoin.sol');
--- web3/truffle.js
+++ web3/truffle.js
@@ -1,9 +1,19 @@
+require('babel-register');
+require('babel-polyfill');
+
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
       network_id: "*", // Match test network id
+      gas: 3000000,
+      gasPrice: 21000000000
+    },
+    testrpc: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*", // Match any network id
       gas: 3000000,
       gasPrice: 21000000000
     }
