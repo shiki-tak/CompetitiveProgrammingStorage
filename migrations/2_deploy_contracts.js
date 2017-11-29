7c0cca85245ed467f8cf0968479f99c83634eb15
user1 <user1@user1noMacBook-Pro.local>
2017-11-29 11:25:19 +0900

    edit migration script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,13 +2,15 @@ const fs = require('fs');
 
 const RuliToken = artifacts.require('RuliToken.sol');
 const RuliCrowdsale = artifacts.require('RuliCrowdsale.sol');
-const tokenParams = JSON.parse(fs.readFileSync('../config/RuliToken.json', 'utf8'));
+// const tokenParams = JSON.parse(fs.readFileSync('../config/RuliToken.json', 'utf8'));
 const crowdsaleParams = JSON.parse(fs.readFileSync('../config/Crowdsale.json', 'utf8'));
 
 module.exports = function deployContracts(deployer) {
 
-  return deployer.deploy(
-    RuliToken, tokenParams.initialAmount, tokenParams.name, tokenParams.decimal,
-    tokenParams.symbol).then(() =>
-  deployer.deploy(RuliCrowdsale, RuliToken.address, crowdsaleParams.fundAddress, crowdsaleParams.offeredAmount, crowdsaleParams.initialRate));
+  deployer.deploy(RuliToken);
+
+  deployer.link(RuliToken, RuliCrowdsale);
+
+  // TODO: start & end block.
+  deployer.deploy(RuliCrowdsale, 900000, 1000000, crowdsaleParams.initialRate, crowdsaleParams.fundAddress));
 };
