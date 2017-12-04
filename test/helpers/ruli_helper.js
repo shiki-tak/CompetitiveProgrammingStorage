5571671f8eb71f813bee374c8866ff70f4cd5d98
user1 <user1@user1noMacBook-Pro.local>
2017-12-04 09:28:15 +0900

    add test: around goal ether amount

--- test/helpers/ruli_helper.js
+++ test/helpers/ruli_helper.js
@@ -6,7 +6,7 @@ const chaiAsPromised = require('chai-as-promised');
 const chaiBigNumber = require('chai-bignumber');
 
 const crowdsaleParams
-= JSON.parse(fs.readFileSync('/Users/shikitakahashi/workspace/ico-contracts/config/Crowdsale.json', 'utf8'));
+= JSON.parse(fs.readFileSync('/Users/user1/Desktop/workspace/ico-contracts/config/Crowdsale.json', 'utf8'));
 
 export const BigNumber = web3.BigNumber;
 export const should = chai
@@ -21,4 +21,4 @@ export const rate = new BigNumber(crowdsaleParams.rate);
 // export const ruliFundAddress = crowdsaleParams.ruliFundAddress;
 export const ruliFundAddress = '0xd34da9604e5e9c2a9cc0aa481b6b24a72af3253b';
 export const initialRuliFundBalance = ruli(crowdsaleParams.initialRuliFundBalance);
-export const goal = ruli(crowdsaleParams.goal);
+export const goal = new BigNumber(crowdsaleParams.goal);
--- test/ruli_crowdsale_refundable.js
+++ test/ruli_crowdsale_refundable.js
@@ -2,17 +2,36 @@ import ether from './helpers/ether';
 import advanceToBlock from './helpers/advanceToBlock';
 import EVMThrow from './helpers/EVMThrow';
 
-import { RuliCrowdsale, cap, rate, initialRuliFundBalance, goal } from './helpers/ruli_helper';
+import { RuliCrowdsale, cap, rate, initialRuliFundBalance, goal, BigNumber } from './helpers/ruli_helper';
 
 contract('RuliCrowdsale', ([owner, wallet, investor]) => {
-  const lessThanGoal = goal.minus(ether(100));
+  const lessThanGoal = ether(goal).minus(ether(100));
 
   beforeEach(async function () {
     this.startBlock = web3.eth.blockNumber + 10;
     this.endBlock = web3.eth.blockNumber + 20;
 
     this.crowdsale = await RuliCrowdsale.new(this.startBlock, this.endBlock, rate, wallet,
-    cap, initialRuliFundBalance, goal, { from: owner });
+    cap, initialRuliFundBalance, ether(goal), { from: owner });
+  });
+
+  describe('creating a valid refundable crowdsale', () => {
+    it('should fail with zero goal', async function () {
+      await RuliCrowdsale.new(this.startBlock, this.endBlock, rate, wallet, cap, initialRuliFundBalance, ether(goal));
+    });
+
+    it('should goal be 700 ether', async function () {
+      const expect = ether(700);
+      const actual = await this.crowdsale.goal();
+      await actual.should.be.bignumber.equal(expect);
+    });
+
+    it('should goal be 105, 000,000 JPY', async function () {
+      const goalAsJPY = new BigNumber(10500000);
+      const expectedEtherPrice = new BigNumber(15000);
+      const convertedGoal = expectedEtherPrice.times(goal);
+      await goalAsJPY.should.be.bignumber.equal(convertedGoal);
+    });
   });
 
   describe('deny refunds', () => {
@@ -24,7 +43,7 @@ contract('RuliCrowdsale', ([owner, wallet, investor]) => {
 
     it('should deny refunds after end if goal was reached', async function () {
       await advanceToBlock(this.startBlock - 1);
-      await this.crowdsale.sendTransaction({ value: goal, from: investor });
+      await this.crowdsale.sendTransaction({ value: ether(goal), from: investor });
       await advanceToBlock(this.endBlock);
       await this.crowdsale.claimRefund({ from: investor }).should.be.rejectedWith(EVMThrow);
     });
@@ -49,14 +68,14 @@ contract('RuliCrowdsale', ([owner, wallet, investor]) => {
   describe('goal was reached', () => {
     it('should forward funds to wallet after end', async function () {
       await advanceToBlock(this.startBlock - 1);
-      await this.crowdsale.sendTransaction({ value: goal, from: investor });
+      await this.crowdsale.sendTransaction({ value: ether(goal), from: investor });
       await advanceToBlock(this.endBlock);
 
       const pre = web3.eth.getBalance(wallet);
       await this.crowdsale.finalize({ from: owner });
       const post = web3.eth.getBalance(wallet);
 
-      post.minus(pre).should.be.bignumber.equal(goal);
+      post.minus(pre).should.be.bignumber.equal(ether(goal));
     });
   });
 });
