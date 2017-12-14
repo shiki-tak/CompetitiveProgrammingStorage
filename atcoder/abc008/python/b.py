a1d7b75b965a182b45e7d9ce9471f2ea9fb25260
user1 <user1@user1noMacBook-Pro.local>
2017-12-14 21:24:01 +0900

    making abc008 b.py

--- atcoder/abc008/python/b.py
+++ atcoder/abc008/python/b.py
@@ -1,1 +1,13 @@
-n = int(input())
+from collections import Counter
+
+N = int(input())
+candidate = [0 for i in range(N)]
+for i in range(N):
+    candidate[i] = input()
+
+count_result = Counter(candidate)
+result = [0 for i in range(N)]
+print(count_result)
+for key, value in count_result.items():
+    print(key)
+    print(value)
