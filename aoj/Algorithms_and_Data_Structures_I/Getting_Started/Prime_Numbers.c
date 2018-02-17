e875bf49e146e41f6c078c938e8ed9bb183e2029
shiki-tak <shiki.tak@gmail.com>
2018-02-17 23:24:31 +0900

    Success Prime_Number & Bubble_Sort

--- aoj/Algorithms_and_Data_Structures_I/Getting_Started/Prime_Numbers.c
+++ aoj/Algorithms_and_Data_Structures_I/Getting_Started/Prime_Numbers.c
@@ -1,31 +1,61 @@
 #include <stdio.h>
 #include <math.h>
 
-void check_prime(int a[], int n) {
-  int count;
+// int check_prime(int a[], int n) {
+//   int cnt = 0;
+//
+//   for (int i = 0; i < a[i]; i++) {
+//     int flag = 1;
+//     for (int j = 2; j <= sqrt(a[i]); j++) {
+//       if (a[i] % j == 0) {
+//         flag = 0;
+//         break;
+//       }
+//     }
+//     if (flag) {
+//       cnt++;
+//     }
+//   }
+//   return cnt;
+// }
+//
+// int main() {
+//   int n;
+//   scanf("%d", &n);
+//   int a[n];
+//   for (int i = 0; i < n; i++) {
+//     scanf("%d", &a[i]);
+//   }
+//   printf("%d\n", check_prime(a, n));
+//   return 0;
+// }
 
-  for (int i = 0; i < a[i]; i++) {
-    int flag = 1;
-    for (int j = 2; j <= sqrt(a[i]); j++) {
-      if (a[i] % j == 0) {
-        flag = 0;
-        break;
-      }
-    }
-    if (flag) {
-      count++;
+int check_prime(int target) {
+  int flag = 1;
+
+  for (int i = 2; i <= sqrt(target); i++) {
+    if (target % i == 0) {
+      flag = 0;
+      break;
     }
   }
-  printf("%d", count);
+  return flag;
 }
 
 int main() {
-  int n;
-  scanf("%d", &n);
+  int n = 0;
+  int cnt = 0;
+  scanf("%d\n", &n);
   int a[n];
   for (int i = 0; i < n; i++) {
-    scanf("%d", &a[i]);
+    scanf("%d\n", &a[i]);
   }
-  check_prime(a, n);
+
+  for (int i = 0; i < n; i++) {
+    if (check_prime(a[i])) {
+      cnt++;
+    }
+  }
+  printf("%d\n", cnt);
   return 0;
 }
