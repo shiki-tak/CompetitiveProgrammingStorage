f2ba08a911c054bdcb67ec4ef3c5579acb1223c9
shiki-tak <shiki.tak@gmail.com>
2018-03-21 10:29:14 +0900

    add todo model

---
+++ db/migrate/20180321012438_create_todos.rb
@@ -0,0 +1,11 @@
+class CreateTodos < ActiveRecord::Migration[5.1]
+  def change
+    create_table :todos do |t|
+      t.string :title
+      t.text :comment
+      t.boolean :done
+
+      t.timestamps
+    end
+  end
+end
