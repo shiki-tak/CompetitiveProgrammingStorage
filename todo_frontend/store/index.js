f1af9b58c7d1e679042ef5167995b008779c9853
shiki-tak <shiki.tak@gmail.com>
2018-03-23 11:07:03 +0900

    add auth

--- todo_frontend/store/index.js
+++ todo_frontend/store/index.js
@@ -2,12 +2,15 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import todo from './modules/todo'
+import auth from './modules/auth'
+
 
 Vue.use(Vuex)
 
 export default () =>
   new Vuex.Store({
     modules: {
-      todo
+      todo,
+      auth
     }
   })
--- todo_frontend/store/mutation-types.js
+++ todo_frontend/store/mutation-types.js
@@ -1,1 +1,11 @@
+// todo
 export const SET_TODOS = 'SET_TODOS'
+
+// user
+export const ACCESS_TOKEN = 'ACCESS_TOKEN'
+export const CLIENT = 'CLIENT'
+export const UID = 'UID'
+export const USER_NAME = 'USER_NAME'
+export const USER_EMAIL = 'USER_EMAIL'
+export const USER_PASSWORD = 'USER_PASSWORD'
+export const SIGNIN_USER = 'SIGNIN_USER'
