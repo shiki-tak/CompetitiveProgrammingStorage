3c7f84493c1d36a344ed72b26d080b7b185454fd
shiki-tak <shiki.tak@gmail.com>
2018-06-19 09:03:11 +0900

    edit Gate.java

--- Learning_from_Java_Introduction_to_Design_Patterns_Multithreaded_Edition/ch1/Gate.java
+++ Learning_from_Java_Introduction_to_Design_Patterns_Multithreaded_Edition/ch1/Gate.java
@@ -1,7 +1,7 @@
 public class Gate {
   private int counter = 0;
   private String name = "Nobody";
-  private String address = "Nowhere";
+  private String address =  "Nowhere";
 
   public synchronized void pass(String name, String address) {
     this.counter++;
@@ -10,13 +10,13 @@ public class Gate {
     check();
   }
 
-  public String toString() {
+  public synchronized String toString() {
     return "No." + counter + ": " + name + ", " + address;
   }
 
-  public synchronized void check() {
+  private void check() {
     if (name.charAt(0) != address.charAt(0)) {
-      System.out.println("**** BROKEN *****" + toString());
+      System.out.println("***** BROKEN ***** " + toString());
     }
   }
 }
