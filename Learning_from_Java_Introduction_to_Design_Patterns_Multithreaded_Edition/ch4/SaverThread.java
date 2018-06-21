5f38dcf52fed055845ec5e21e961b1eb4df6cc3c
shiki-tak <shiki.tak@gmail.com>
2018-06-21 10:02:19 +0900

    edit gitignore and delete txt

---
+++ Learning_from_Java_Introduction_to_Design_Patterns_Multithreaded_Edition/ch4/SaverThread.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+
+public class SaverThread extends Thread {
+  private final Data data;
+
+  public SaverThread(String name, Data data) {
+    super(name);
+    this.data = data;
+  }
+
+  public void run() {
+    try {
+      while(true) {
+        data.save();
+        Thread.sleep(100);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+}
