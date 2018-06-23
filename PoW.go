4ff2a66cdd3083e5bddc40a2a6a8bf5b7e6d0bd1
shiki-tak <shiki.tak@gmail.com>
2018-06-24 00:26:44 +0900

    first commit

---
+++ PoW.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"time"
+)
+
+// Block
+type Block struct {
+	PreviousHash string
+	BlockHash    string
+	MerkleRoot   string
+	Nonce        int
+	TimeStamp    int64
+}
+
+var blockChain []Block
+
+func createBlock(previousHash string, blockHash string, merkleRoot string, nonce int, timeStamp int64) {
+
+	block := Block{
+		PreviousHash: previousHash,
+		BlockHash:    blockHash,
+		MerkleRoot:   merkleRoot,
+		Nonce:        nonce,
+		TimeStamp:    timeStamp,
+	}
+
+	blockChain = make([]Block, 0, 10)
+	blockChain = append(blockChain, block)
+	blockAsJSON, err := json.Marshal(block)
+	if err != nil {
+		fmt.Printf("error at creating genesis block: %v", err)
+	}
+	fmt.Printf("%s\n", blockAsJSON)
+}
+
+// Calculate the hash
+func calcHash(previousHash string, merkleRoot string, nonce string, timeStamp int64) string {
+
+	result := sha256.Sum256([]byte(previousHash + merkleRoot + nonce + strconv.FormatInt(timeStamp, 10)))
+	resultAsString := hex.EncodeToString(result[:])
+	return resultAsString
+
+}
+
+func matchTargetCondition(target string, calcResult string) bool {
+	if target == calcResult[:len(target)] {
+		return true
+	}
+	return false
+}
+
+// Execute Proof Of Work
+func pow(merkleRoot string, timeStamp int64) bool {
+	target := "0000"
+
+	nonce := 0
+	fmt.Println(string(nonce))
+
+	// Latest Block Height Index
+	i := len(blockChain) - 1
+
+	for {
+		calcResult := calcHash(blockChain[i].PreviousHash, merkleRoot, string(nonce), timeStamp)
+		if matchTargetCondition(target, calcResult) {
+			fmt.Println(calcResult)
+			createBlock(
+				blockChain[i].BlockHash,
+				calcResult,
+				merkleRoot,
+				nonce,
+				timeStamp,
+			)
+			break
+		}
+		nonce++
+	}
+	return true
+}
+
+func main() {
+
+	var merkleRoot string
+
+	// TODO:
+	// calculate merkle root
+
+	// i: block number
+	for i := 0; i < 10; i++ {
+		if i == 0 {
+			// Create genesis block
+			blockHashAsByte := sha256.Sum256([]byte("genesis block"))
+
+			createBlock(
+				"0x0000000000000000000000000000000000000000000000000000000000000000",
+				hex.EncodeToString(blockHashAsByte[:]),
+				merkleRoot,
+				42,
+				time.Now().Unix(),
+			)
+		} else {
+			pow(merkleRoot, time.Now().Unix())
+		}
+	}
+}
