f3df1d9cafacc098a53f23c74aede89c40f58ef0
shiki-tak <shiki.tak@gmail.com>
2018-06-26 11:31:08 +0900

    edit root tree

--- aoj/Algorithms_and_Data_Structures_I/07.Tree/Rooted_Trees.go
+++ aoj/Algorithms_and_Data_Structures_I/07.Tree/Rooted_Trees.go
@@ -2,9 +2,24 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 )
+
+// 接点の構造体
+type Node struct {
+	p int
+	l int
+	r int
+}
+
+// 接点を入れるための配列
+var T []Node
+
+func output(u int) {
+
+}
 
 var sc = bufio.NewScanner(os.Stdin)
 
@@ -20,8 +35,18 @@ func nextInt() int {
 func main() {
 	sc.Split(bufio.ScanWords)
 	n := nextInt()
-	a := make([]int, n)
+	s := make([]int, 0)
+	T = make([]Node, 10)
+
 	for i := 0; i < n; i++ {
-		a[i] = nextInt()
+		T[i].p = -1
+		T[i].l = -1
+		T[i].r = -1
+	}
+	for i := 0; i < n; i++ {
+		s = append(s, nextInt())
+	}
+	for i := 0; i < n; i++ {
+		fmt.Println(s[i])
 	}
 }
