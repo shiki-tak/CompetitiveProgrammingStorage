75d5be1b0b073be99693143095a934dcfb0c2cda
shiki-tak <shiki.tak@gmail.com>
2018-07-03 10:45:26 +0900

    add shutdown hook

--- Learning_from_Java_Introduction_to_Design_Patterns_Multithreaded_Edition/ch10/Main.java
+++ Learning_from_Java_Introduction_to_Design_Patterns_Multithreaded_Edition/ch10/Main.java
@@ -1,24 +1,53 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("main: BEGIN");
-    try {
-      // スレッドの起動
-      CountupThread t = new CountupThread();
-      t.start();
 
-      Thread.sleep(10000);
+    // キャッチされない例外のハンドラを設定する
+    Thread.setDefaultUncaughtExceptionHandler (
+      new Thread.UncaughtExceptionHandler() {
+        public void uncaughtException(Thread thread, Throwable exception) {
+          System.out.println("****");
+          System.out.println("UncaughtExceptionHandler: BEGIN");
+          System.out.println("currentThread = " + Thread.currentThread());
+          System.out.println("thread = " + exception);
+          System.out.println("UncaughtExceptionHandler: END");
+        }
+      }
+    );
 
-      // スレッドの終了要求
-      System.out.println("main: shutdownRequest");
-      t.shutdownRequest();
+    // シャットダウン・フックを設定する
+    Runtime.getRuntime().addShutdownHook (
+      new Thread() {
+        public void run() {
+          System.out.println("****");
+          System.out.println("shutdown hook:BEGIN");
+          System.out.println("currentThread = " + Thread.currentThread());
+          System.out.println("shutdown hook: END");
+        }
+      }
+    );
 
-      System.out.println("main: join");
+    // 約3秒後に「0による整数の割り算」を行うスレッドを起動する
+    new Thread("MyThread") {
+      public void run() {
+        System.out.println("MyThread: BEGIN");
+        System.out.println("MyThread: SLEEP...");
 
-      // スレッドの終了を待つ
-      t.join();
-    } catch(InterruptedException e) {
-      e.printStackTrace();
-    }
+        try {
+          Thread.sleep(3000);
+        } catch (InterruptedException e) {
+
+        }
+        System.out.println("MyThread: DIVIDE");
+
+        // 「0による整数の割り算」
+        int x = 1 / 0;
+
+        // 到達しない
+        System.out.println("MyThread: END");
+      }
+    }.start();
+
     System.out.println("main: END");
   }
 }
