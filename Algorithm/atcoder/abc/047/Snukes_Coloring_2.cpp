66d2dfcaf64433eea322e91629a3942c7c23b329
shiki-tak <shiki.tak@gmail.com>
2018-07-10 11:17:49 +0900

    remove Swift

---
+++ Algorithm/atcoder/abc/047/Snukes_Coloring_2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+  int w, h, n;
+  cin >> w >> h >> n;
+  for (int i = 0; i < n; i++) {
+
+  }
+
+  return 0;
+}
