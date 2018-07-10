434908487cd2557beb5496e9a3a7d9778adff134
shiki-tak <shiki.tak@gmail.com>
2018-07-10 17:53:15 +0900

    atcoder abc 047 b halfway

--- Algorithm/atcoder/abc/047/Snukes_Coloring_2.cpp
+++ Algorithm/atcoder/abc/047/Snukes_Coloring_2.cpp
@@ -5,10 +5,33 @@ using std::cout;
 using std::endl;
 
 int main() {
-  int w, h, n;
+  int w, h, n, s;
+  // 長方形を作るx座標の左端、右端を表す（xp[0]: 右, xp[1]: 左）
+  int xp[2];
+  // 長方形を作るy座標の上端、下端を表す（yp[0]: 上, xp[1]: 下）
+  int yp[2];
   cin >> w >> h >> n;
-  for (int i = 0; i < n; i++) {
 
+  xp[0] = w;
+  xp[1] = 0;
+  yp[0] = h;
+  yp[1] = 0;
+
+  for (int i = 0; i < n; i++) {
+    int x, y, a;
+    cin >> x >> y >> a;
+
+    if (a == 1)      xp[1] = x;
+    else if (a == 2) xp[0] = x;
+    else if (a == 3) yp[1] = y;
+    else             yp[0] = y;
+  }
+
+  if ((xp[0] - xp[1]) <= 0 || (yp[0] - yp[1] <= 0)) {
+    cout << 0 << endl;
+  } else {
+    s = (xp[0] - xp[1]) * (yp[0] - yp[1]);
+    cout << s << endl;
   }
 
   return 0;
