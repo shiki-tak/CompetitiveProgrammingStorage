d0cfef457546bc506820930b9d36872e0c4d226e
shiki-tak <shiki.tak@gmail.com>
2018-07-10 09:25:57 +0900

    add ico-contracts

--- rails-truffle-sample/migrations/2_deploy_greeting_contract.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-const HelloWorld = artifacts.require('GreetingContract.sol');
-module.exports = function deployContracts(deployer) {
-  deployer.deploy(HelloWorld, 'Hello, World');
-}
