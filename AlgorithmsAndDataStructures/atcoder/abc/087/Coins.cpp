048cf134ccadeb9746f732751d9d89449c33ebde
shiki-tak <shiki.tak@gmail.com>
2018-07-14 16:13:02 +0900

    atcoder abc 087 b

---
+++ AlgorithmsAndDataStructures/atcoder/abc/087/Coins.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int a, b, c, x, ans;
+  cin >> a >> b >> c >> x;
+
+  for (int i = 0; i <= a; i++) {
+    for (int j = 0; j <= b; j++) {
+      for (int k = 0; k <= c; k++) {
+        if (500 * i + 100 * j + 50 * k == x) {
+          ans++;
+        }
+      }
+    }
+  }
+
+  cout << ans << endl;
+
+  return 0;
+}
