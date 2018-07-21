cc873b485568bf6ebf76c4870ebb6ff2ce54ef7e
shiki-tak <shiki.tak@gmail.com>
2018-07-21 22:17:01 +0900

    atcoder abc 103 b

---
+++ AlgorithmsAndDataStructures/atcoder/abc/103/String_Rotation.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string s, t;
+
+int rotation() {
+
+    char x = s[s.length() - 1];
+
+    for (int i = s.length() - 1; i >= 0; i--) {
+        if (i == 0) {
+            s[i] = x;
+        } else {
+        s[i] = s[i - 1];
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+    cin >> s >> t;
+    string ret = "No";
+
+    for (int i = 0; i < s.length(); i++) {
+        rotation();
+        if (s == t) {
+            ret = "Yes";
+            break;
+        }
+    }
+
+    cout << ret << endl;
+
+    return 0;
+}
