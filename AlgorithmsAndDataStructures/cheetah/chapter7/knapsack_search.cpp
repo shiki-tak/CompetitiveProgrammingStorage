ffd809c19d776dc13cded4b416c6d0cfd1afa0b2
shiki-tak <shiki.tak@gmail.com>
2018-07-23 00:42:12 +0900

    add cheetah chapter7 knapsack_search.cpp

--- AlgorithmsAndDataStructures/cheetah/chapter7/knapsack_search.cpp
+++ AlgorithmsAndDataStructures/cheetah/chapter7/knapsack_search.cpp
@@ -2,18 +2,24 @@
 
 using namespace std;
 
-int weight = {3, 4, 1, 2, 3};
-int value = {2, 3, 2, 3, 6};
+int weight[] = {3, 4, 1, 2, 3};
+int value[] = {2, 3, 2, 3, 6};
 
-int search(int w, int v) {
+// 分割統治法でナップサック問題を解く
+int search(int i, int w, int sum) {
+    // cout << "i: " << i << ", " << "w: " << w << " , sum: " << sum << endl;
+
+    if (i > 4)   return sum;
+    if (w >= 10) return  sum;
+
+    int ret = max(search(i + 1, w, sum), search(i + 1, w + weight[i], sum + value[i]));
 
     return ret;
 }
 
 int main()
 {
-
-    search(0, 0);
+    cout << search(0, 0, 0) << endl;
 
     return 0;
 }
