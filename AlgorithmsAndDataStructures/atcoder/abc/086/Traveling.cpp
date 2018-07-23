deba082749f10b0455752401bcea3167040d617f
shiki-tak <shiki.tak@gmail.com>
2018-07-23 20:22:21 +0900

    edit atcoder abc 086 c

--- AlgorithmsAndDataStructures/atcoder/abc/086/Traveling.cpp
+++ AlgorithmsAndDataStructures/atcoder/abc/086/Traveling.cpp
@@ -6,33 +6,19 @@ using namespace std;
 int main()
 {
     int n; cin >> n;
-    int t[n], x[n], y[n];
+    int t[n + 1], x[n + 1], y[n + 1];
+    t[0] = 0, x[0] = 0, y[0] = 0;
 
     bool traveling = true;
 
-    for (int i = 0; i < n; i++) cin >> t[i] >> x[i] >> y[i];
+    for (int i = 1; i <= n; i++) {
+        cin >> t[i] >> x[i] >> y[i];
+        // 時刻iと時刻i-1の差分を計算
+        int dt = t[i] - t[i - 1];
+        int ds = abs(x[i] - x[i - 1]) + abs(y[i] - y[i - 1]);
 
-    for (int i = 0; i < n; i++) {
-
-        int dt = 0, ds = 0;
-
-        if (i == 0) {
-            dt = t[i];
-            ds = x[i] + y[i];
-        } else {
-            dt = t[i] - t[i - 1];
-            ds = abs(x[i] - x[i - 1]) + abs(y[i] - y[i - 1]);
-        }
-        if (dt < ds) {
-            traveling = false;
-            break;
-        } else if (ds % 2 == 0 && dt % 2 != 0) {
-            traveling = false;
-            break;
-        } else if (ds % 2 != 0 && dt % 2 == 0) {
-            traveling = false;
-            break;
-        }
+        // 旅行がプラン実行が不可能な場合
+        if ((dt < ds) || (ds % 2 == 0 && dt % 2 != 0) || (ds % 2 != 0 && dt % 2 == 0)) traveling = false;
     }
 
     if (traveling) cout << "Yes" << endl;
