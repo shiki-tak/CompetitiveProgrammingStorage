a62777800e0240a748d12f9a5ef02b91a31c6c5b
shiki-tak <shiki.tak@gmail.com>
2018-08-03 08:36:16 +0900

    edit atcoder abc 083 a

--- AlgorithmsAndDataStructures/atcoder/abc/083/Libra.cpp
+++ AlgorithmsAndDataStructures/atcoder/abc/083/Libra.cpp
@@ -5,8 +5,7 @@ using namespace std;
 int main()
 {
     int a, b, c, d; cin >> a >> b >> c >> d;
-    if (a + b > c + d)      cout << "Left" << endl;
-    else if (a + b < c + d) cout << "Right" << endl;
-    else                    cout << "Balanced" << endl;
+    // 三項演算子をネストさせてみた
+    cout << (a + b == c + d ? "Balanced" : a + b > c + d ? "Left" : "Right") << endl;
     return 0;
 }
