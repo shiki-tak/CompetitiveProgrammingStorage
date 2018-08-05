2f97eedfa90a8a733aa28c1a8e677443942b792b
shiki-tak <shiki.tak@gmail.com>
2018-08-05 11:24:43 +0900

    edit cheetah chapter7 knapsack_df.cpp

--- AlgorithmsAndDataStructures/cheetah/chapter7/knapsack_df.cpp
+++ AlgorithmsAndDataStructures/cheetah/chapter7/knapsack_df.cpp
@@ -9,19 +9,23 @@ int main()
     int ws[5] = {3, 4, 1, 2, 3};
     int ps[5] = {2, 3, 2, 3, 6};
     int maxw = 10;
+    // dp[x][y] ... x: x番目の価値, y: 重さ
     int dp[6][11];
     int ret = 0;
 
+    // 初期化
+    for (int i = 0; i <= maxw; i++) dp[0][i] = 0;
+
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j <= maxw; j++) {
-            if (j + ws[i] <= maxw) {
-                dp[i + 1][j + ws[i]] = max(dp[i + 1][j + ws[i]], dp[i][j] + ps[j]);
-                ret = max(dp[i + 1][j + ws[i]], ret);
+            if (j >= ws[i]) {
+                dp[i + 1][j] = max(dp[i][j - ws[i]] + ps[i], dp[i][j]);
+            } else {
+                dp[i + 1][j] = dp[i][j];
             }
         }
     }
 
-    cout << ret << endl;
-
+    cout << dp[5][10] << endl;
     return 0;
 }
