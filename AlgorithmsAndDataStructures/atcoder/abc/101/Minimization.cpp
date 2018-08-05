c66af5e0a457b6f1af17d0d2a270a9589b6656f5
shiki-tak <shiki.tak@gmail.com>
2018-08-05 09:51:48 +0900

    atcoder abc 101 c

---
+++ AlgorithmsAndDataStructures/atcoder/abc/101/Minimization.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, k; cin >> n >> k;
+    int cnt = 0;
+    int a[100010] = {0};
+
+    // 最小値である1がどこにあるかに関係なく、置き換える回数が決まる
+    cnt = ((n - 1) + (k - 1) - 1) / (k - 1); 
+
+    // よって、配列への標準入力は不要なのだが...
+    for (int i = 1; i <= n; i++) cin >> a[i];
+    cout << cnt << endl;
+
+    return 0;
+}
