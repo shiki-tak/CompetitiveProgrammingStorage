15d0e6d6bac176255cb63a9f847d39463689b083
shiki-tak <shiki.tak@gmail.com>
2018-08-13 12:26:28 +0900

    edit Main.java

--- src/main/java/trie/Main.java
+++ src/main/java/trie/Main.java
@@ -1,6 +1,18 @@
 package trie;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 	public static void main(String[] args) {
+		List<MerkleHash> merkleHashList = new ArrayList<>();
+		merkleHashList.add(new MerkleHash("L1"));
+		merkleHashList.add(new MerkleHash("L2"));
+		merkleHashList.add(new MerkleHash("L3"));
+		merkleHashList.add(new MerkleHash("L4"));
+
+		MerkleTree merkleTree = MerkleTree.getMerkleTree(merkleHashList);
+
+		System.out.println("root: " + merkleTree.getMerkleHash().sha256HexBinary());
 	}
 }
