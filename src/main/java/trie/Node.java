79839b1496742ad6d9581a49ce18b763ba91f2ef
shiki-tak <shiki.tak@gmail.com>
2018-08-13 11:51:31 +0900

    edit Node.java

--- src/main/java/trie/Node.java
+++ src/main/java/trie/Node.java
@@ -3,9 +3,12 @@ package trie;
 import javax.validation.constraints.NotNull;
 
 public class Node extends MerkleTree {
+	private MerkleTree left;
+	private MerkleTree right;
 
 	public Node(@NotNull MerkleHash merkleHash, @NotNull MerkleTree left, @NotNull MerkleTree right) {
 		super(merkleHash);
+		this.left = left;
+		this.right = right;
 	}
-
 }
