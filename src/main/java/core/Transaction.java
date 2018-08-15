ad0b8ff5128cabf3f02824e36235e61ce7f80b0d
shiki-tak <shiki.tak@gmail.com>
2018-08-15 14:58:09 +0900

    add field in Transaction.java

--- src/main/java/core/Transaction.java
+++ src/main/java/core/Transaction.java
@@ -3,9 +3,38 @@ package core;
 import java.util.ArrayList;
 import java.util.List;
 
+import accounts.Address;
 import trie.BloomFilter;
 
 public class Transaction {
+
+	// Transactionの構造
+	private Address to;		// 受信者
+	private Address from;	// 送信者
+	private int value;		// 送信者から受領人へ送られるETHの量
+	private String data;	// オプショナルフィールド
+	private int gasLimit;	// トランザクションの実行にかかる計算のステップ数の最大値
+	private int gasPrice;	// 送信者が支払う、１計算ステップあたりの手数料
+	private int nonce;		// fromが今までに実行したトランザクションの回数
+
+	// getter
+	public Address getTo() { return to; }
+	public Address getFrom() { return from; }
+	public int getValue() { return value; }
+	public String getData() { return data; }
+	public int getGasLimit() { return gasLimit; }
+	public int getGasPrice() { return gasPrice; }
+	public int getNonce() { return nonce; }
+
+	// setter
+	public void setTo(Address to) { this.to = to; }
+	public void setFrom(Address from) { this.from = from; }
+	public void setValue(int value) { this.value = value; }
+	public void setData(String data) { this.data = data; }
+	public void setGasLimit(int gasLimit) { this.gasLimit = gasLimit; }
+	public void setGasPrice(int gasPrice) { this.gasPrice = gasPrice; }
+	public void setNonce(int nonce) { this.nonce = nonce; }
+
 	// TODO: Transactionクラスの実装
 	public static List<String> generateTransaction(int n) {
 		List<String> txs = new ArrayList<>();
