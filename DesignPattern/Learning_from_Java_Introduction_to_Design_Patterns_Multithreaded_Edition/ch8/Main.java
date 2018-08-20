31794acfccc0fc43bd188499a23e86b99cfde313
shiki-tak <shiki.tak@gmail.com>
2018-08-20 10:57:11 +0900

    delete

--- DesignPattern/Learning_from_Java_Introduction_to_Design_Patterns_Multithreaded_Edition/ch8/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-  public static void main(String[] args) {
-    Channel channel = new Channel(5);
-    channel.startWorkers();
-    new ClientThread("Alice", channel).start();
-    new ClientThread("Bobby", channel).start();
-    new ClientThread("Chris", channel).start();
-  }
-}
