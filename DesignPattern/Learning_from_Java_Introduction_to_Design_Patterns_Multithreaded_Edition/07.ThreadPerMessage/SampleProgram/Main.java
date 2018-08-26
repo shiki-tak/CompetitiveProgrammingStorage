f802810ea9d784f0c020eaa0456016abc24be3c8
shiki-tak <shiki.tak@gmail.com>
2018-08-26 09:53:34 +0900

    edit DesignPattern chapter7 sample

--- DesignPattern/Learning_from_Java_Introduction_to_Design_Patterns_Multithreaded_Edition/07.ThreadPerMessage/SampleProgram/Main.java
+++ DesignPattern/Learning_from_Java_Introduction_to_Design_Patterns_Multithreaded_Edition/07.ThreadPerMessage/SampleProgram/Main.java
@@ -1,7 +1,17 @@
+import java.util.concurrent.ThreadFactory;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("main BEGIN");
-        Host host = new Host();
+        Host host = new Host(
+            new ThreadFactory(){
+            
+                @Override
+                public Thread newThread(Runnable r) {
+                    return new Thread(r);
+                }
+            }
+        );
         host.request(10, 'A');
         host.request(20, 'B');
         host.request(30, 'C');
