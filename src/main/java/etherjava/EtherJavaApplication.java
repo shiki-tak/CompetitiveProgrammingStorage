3b10dfa8dfa4a22179fb3ee4144046d71c8c30c9
shiki-tak <shiki.tak@gmail.com>
2018-09-13 14:28:36 +0900

    first commit

---
+++ src/main/java/etherjava/EtherJavaApplication.java
@@ -0,0 +1,12 @@
+package etherjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtherJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtherJavaApplication.class, args);
+	}
+}
