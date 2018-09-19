7d2f366d56d36a73393c90fadabe0afab547a10b
shiki-tak <shiki.tak@gmail.com>
2018-09-19 16:31:10 +0900

    ant 2386

--- AlgorithmsAndDataStructures/ant/2386/LakeCounting.cpp
+++ AlgorithmsAndDataStructures/ant/2386/LakeCounting.cpp
@@ -2,16 +2,42 @@
 
 using namespace std;
 
-int lc[110][110];
+char lc[110][110];
+int n = 0;
+int m = 0;
+
+int dx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+int dy[8] = {1, 1, 0, -1, -1, -1, 0, 1}; 
+
+void dfs(int x, int y) {
+    lc[x][y] = '.';
+    for (int i = 0; i < 8; i++) {
+        if (lc[x + dx[i]][y + dy[i]] == 'W' && x + dx[i] < n && y + dy[i] < m && x + dx[i] >= 0 && y + dy[i] >= 0) {
+            dfs(x + dx[i], y + dy[i]);
+        }
+    }
+}
 
 int main()
 {
-    int n, m; cin >> n >> m;
+    cin >> n >> m;
+    int ans = 0;
+
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            cint >> ls[i][j];
-
+            cin >> lc[i][j];
         }
     }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (lc[i][j] == 'W') {
+                dfs(i, j);
+                ans++;
+            }
+        }
+    }
+
+    cout << ans << endl;
+
     return 0;
 }
