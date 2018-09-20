1eacf4886a57f5f897f2f6568e25acb074a544fe
shiki-tak <shiki.tak@gmail.com>
2018-09-20 16:21:17 +0900

    refactoring

--- AlgorithmsAndDataStructures/atcoder/atc/001/bfs.cpp
+++ AlgorithmsAndDataStructures/atcoder/atc/001/bfs.cpp
@@ -9,19 +9,16 @@ int dx[4] = {0, 1, 0, -1};
 int dy[4] = {1, 0, -1, 0}; 
 
 bool bfs(int x, int y, int ans) {
-    if (citySection[x][y] == 'g') {
-        ans = true;
-        return ans;
-    } else {
-        citySection[x][y] = 'E';
-    }
+    citySection[x][y] = 'E';
 
     for (int i = 0; i < 4; i++) {
-        if ( x + dx[i] < 0 || x + dx[i] >= h) { continue; }
-        if ( y + dy[i] < 0 || y + dy[i] >= w) { continue; }
-        if (citySection[x + dx[i]][y + dy[i]] == '.' || citySection[x + dx[i]][y + dy[i]] == 'g') {
-
-            ans = bfs(x + dx[i], y + dy[i], ans);
+        if (x + dx[i] >= 0 && x + dx[i] < h && y + dy[i] >= 0 && y + dy[i] < w) {
+            if (citySection[x + dx[i]][y + dy[i]] == '.') {
+                ans = bfs(x + dx[i], y + dy[i], ans);
+            } else if (citySection[x + dx[i]][y + dy[i]] == 'g') {
+                ans = true;
+                return ans;
+            }
         }
     }
 
@@ -35,23 +32,10 @@ int main()
     for (int i = 0; i < h; i++) {
         for (int j = 0; j < w; j++) {
             cin >> citySection[i][j];
-            if (citySection[i][j] == 's') {
-                sX = i;
-                sY = j;
-            }
+            if (citySection[i][j] == 's') { sX = i; sY = j; }
         }
     }
     cout << (bfs(sX, sY, false) ? "Yes" : "No") << endl;
-
-    // for (int i = 0; i < h; i++) {
-    //     for (int j = 0; j < w; j++) {
-    //         if (j == w - 1) {
-    //             cout << citySection[i][j] << endl;
-    //         } else {
-    //             cout << citySection[i][j];
-    //         }
-    //     }
-    // }
 
     return 0;
 }
