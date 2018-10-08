1565e8ea870eab5891e845e083da9fc577cebba1
shiki-tak <shiki.tak@gmail.com>
2018-10-08 15:17:00 +0900

    edit atcoder abc 086 a c++

--- AlgorithmsAndDataStructures/atcoder/abc/086/c++/Product.cpp
+++ AlgorithmsAndDataStructures/atcoder/abc/086/c++/Product.cpp
@@ -4,8 +4,6 @@ using namespace std;
 
 int main()
 {
-  int a, b;
-  cin >> a >> b;
-  if (a * b % 2 == 0) cout << "Even" << endl;
-  else                cout << "Odd" << endl;
+  int a, b; cin >> a >> b;
+  cout << (a * b % 2 == 0 ? "Even" : "Odd") << endl;
 }
