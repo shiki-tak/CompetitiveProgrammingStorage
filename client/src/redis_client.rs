607712f9e4b389a33ed90ab5fe1f0832d93a480b
shiki <shiki.tak@gmail.com>
2019-05-19 08:55:06 +0900

    succeeded in registering task in redis

--- client/src/redis_client.rs
+++ client/src/redis_client.rs
@@ -3,16 +3,21 @@ use redis::Commands;
 
 use model::task::Task;
 
-pub fn set(id: i32, data: &Task) {
-        let json = serde_json::to_string(&data).unwrap();
+pub fn set(id: i32, task: &Task) {
+    let client = redis::Client::open("redis://127.0.0.1:6379/").unwrap();
+    let con = client.get_connection().unwrap();
 
-    // let client = redis::Client::open("redis://127.0.0.1/");
-    // let con = client.get_connection();
-
-    // let _ : () = con.set(id, task);
-
+    let _ : () = con.set(id, serde_json::to_string(&task).unwrap()).unwrap();
 }
 
 pub fn get() {
+
+}
+
+pub fn gets() {
+
+}
+
+pub fn delete() {
 
 }
