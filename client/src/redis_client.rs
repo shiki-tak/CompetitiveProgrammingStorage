cc9a42ae54618e9bb43a1d677e2c15e74f4c5f9d
shiki <shiki.tak@gmail.com>
2019-05-19 20:42:30 +0900

    refactor redis_client

--- client/src/redis_client.rs
+++ client/src/redis_client.rs
@@ -15,15 +15,16 @@ lazy_static! {
 }
 
 pub fn set(id: i32, task: &Task) {
-    let key = PREFIX.get(&0).unwrap().to_string() + &id.to_string();
-
-    let _ : () = generate_client().set(key, serde_json::to_string(&task).unwrap()).unwrap();
+    let _ : () = generate_client()
+                    .set(generate_key(id),
+                        serde_json::to_string(&task).unwrap())
+                    .unwrap();
 }
 
 pub fn get(id: i32) -> Task {
-    let key = PREFIX.get(&0).unwrap().to_string() + &id.to_string();
-    let res: String = generate_client().get(key).unwrap();
-
+    let res: String = generate_client()
+                        .get(generate_key(id))
+                        .unwrap();
     serde_json::from_str(&res).unwrap()
 }
 
@@ -33,7 +34,9 @@ pub fn gets() -> Vec<Task> {
     let key_pattern = PREFIX.get(&0).unwrap().to_string() + &"*".to_string();
     let mut tasks: Vec<Task> = Vec::new();
     // FIXME: use scan
-    let task_keys: Vec<String> = generate_client().keys(key_pattern).unwrap();
+    let task_keys: Vec<String> = generate_client()
+                                    .keys(key_pattern)
+                                    .unwrap();
     for key in task_keys {
         let task: String = con.get(key).unwrap();
         tasks.push(serde_json::from_str(&task).unwrap());
@@ -42,12 +45,16 @@ pub fn gets() -> Vec<Task> {
 }
 
 pub fn delete(id: i32) {
-    let key = PREFIX.get(&0).unwrap().to_string() + &id.to_string();
-
-    let _ : () = generate_client().del(key).unwrap();
+    let _ : () = generate_client()
+                    .del(generate_key(id))
+                    .unwrap();
 }
 
 fn generate_client() -> Connection {
     let client = redis::Client::open("redis://127.0.0.1:6379/").unwrap();
     client.get_connection().unwrap()
+}
+
+fn generate_key(id: i32) -> String {
+    PREFIX.get(&0).unwrap().to_string() + &id.to_string()
 }
