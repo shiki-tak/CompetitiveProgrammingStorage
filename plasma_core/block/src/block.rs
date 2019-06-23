6954d08a702e11893180bf6d3257e15060902fbd
shiki <shiki.tak@gmail.com>
2019-06-23 16:14:15 +0900

    add spent_utxos in block

--- plasma_core/block/src/block.rs
+++ plasma_core/block/src/block.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 // FIXME: create constants.rs
 pub const NULL_SIG: &'static str = "00000000000000000000000000000000000000000000000000000000000000000";
 
@@ -6,10 +8,11 @@ pub struct Block {
     pub transactoin_set: Vec<String>,
     pub number: i32,
     pub sig: String,
+    pub spent_utxos: HashMap<String, i64>
 }
 
 impl Block {
     pub fn new(transactoin_set: Vec<String>) -> Self {
-        Self { transactoin_set: transactoin_set, number: 0, sig: NULL_SIG.to_string() }
+        Self { transactoin_set: transactoin_set, number: 0, sig: NULL_SIG.to_string(), spent_utxos: HashMap::new() }
     }
 }
