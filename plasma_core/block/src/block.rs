d0e1967dd4f2941cefac12e1f3092a3114b560f2
shiki <shiki.tak@gmail.com>
2019-06-26 01:07:00 +0900

    fix return value type in block.rs

--- plasma_core/block/src/block.rs
+++ plasma_core/block/src/block.rs
@@ -11,34 +11,32 @@ pub const NULL_SIG: &'static str = "00000000000000000000000000000000000000000000
 pub struct Block {
     pub transactoin_set: Vec<Transaction>,
     pub number: i32,
-    pub sig: String,
+    pub sig: Vec<u8>,
     pub spent_utxos: HashMap<String, i64>,
     pub hash: String,
     pub signer: String,
     pub merkle: FixedMerkle,
     pub root: String,
     pub is_deposit_block: bool,
-    pub encoded: Vec<u8>,
+    pub encoded: String,
 }
 
 impl Block {
     pub fn new(transactoin_set: Vec<Transaction>) -> Self {
-        let encoded: Vec<u8> = Vec::new();  // FIXME: use rlp module
-
         Self { transactoin_set: transactoin_set,
         number: 0,
-        sig: NULL_SIG.to_string(),
+        sig: NULL_SIG.to_string().as_bytes().to_vec(),
         spent_utxos: HashMap::new(),
         hash: "".to_string(),
         signer: "".to_string(),
         merkle: FixedMerkle::new(),
         root: "".to_string(),
         is_deposit_block: false,
-        encoded: encoded
+        encoded: "".to_string()
         }
     }
 
-    pub fn sign(&self, key: String) -> String {
+    pub fn sign(&self, key: String) -> Vec<u8> {
         signatures::sign(&key)
     }
 
