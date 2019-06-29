02d6536e7fba76063e93a16f31031fa520b7f88d
shiki <shiki.tak@gmail.com>
2019-06-29 11:41:57 +0900

    event watcher done

--- event-watcher/src/main.rs
+++ event-watcher/src/main.rs
@@ -1,32 +1,83 @@
 use std::time::Duration;
 use tokio::prelude::*;
 use tokio::timer::Interval;
-use ethabi::{Event, EventParam, ParamType};
+use ethabi::{Event, EventParam, ParamType, Hash, Topic, TopicFilter};
 use ethereum_types::Address;
 use futures::try_ready;
 use futures::{Async, Future, Poll, Stream};
 use web3::types::{BlockNumber, FilterBuilder, Log};
 use web3::{transports, Web3};
+use std::collections::HashMap;
 
-pub struct EventFetcher {
+// Event DB
+#[derive(Default)]
+pub struct DefaultEventDb {
+    last_logged_blocks: HashMap<Hash, u64>,
+}
+
+pub trait EventDb {
+    fn get_last_logged_block(&self, topic_hash: Hash) -> Option<u64>;
+    fn set_last_logged_block(&mut self, topic_hash: Hash, block_number: u64);
+}
+
+impl DefaultEventDb {
+    pub fn new() -> DefaultEventDb {
+        Default::default()
+    }
+}
+
+impl EventDb for DefaultEventDb {
+    fn get_last_logged_block(&self, topic_hash: Hash) -> Option<u64> {
+        match self.last_logged_blocks.get(&topic_hash) {
+            Some(block_number) => Some(*block_number),
+            None => None,
+        }
+    }
+
+    fn set_last_logged_block(&mut self, topic_hash: Hash, block_number: u64) {
+        self.last_logged_blocks.insert(topic_hash, block_number);
+    }
+}
+
+// Event Watcher
+pub struct EventFetcher<T> where T: EventDb, {
     interval: Interval,
     web3: Web3<transports::Http>,
     address: Address,
     abi: Vec<Event>,
+    db: T,
 }
 
-impl EventFetcher {
-    pub fn new(web3: Web3<transports::Http>, address: Address, abi: Vec<Event>) -> Self {
+impl<T> EventFetcher<T> where T: EventDb, {
+    pub fn new(web3: Web3<transports::Http>, address: Address, abi: Vec<Event>, db: T) -> Self {
         EventFetcher {
             interval: Interval::new_interval(Duration::from_secs(1)),
             address,
             abi,
             web3,
+            db,
+        }
+    }
+
+    fn filter_events(&self, event: &Event, logs: Vec<Log>) -> Vec<Log> {
+        if let Some(last_logged_block) = self.db.get_last_logged_block(event.signature()) {
+            logs.iter()
+                .filter(|&log| {
+                    if let Some(n) = log.block_number {
+                        n.low_u64() > last_logged_block
+                    } else {
+                        false
+                    }
+                })
+                .cloned()
+                .collect()
+        } else {
+            logs.clone()
         }
     }
 }
 
-impl Stream for EventFetcher {
+impl<T> Stream for EventFetcher<T> where T: EventDb, {
     type Item = Vec<Log>;
     type Error = ();
 
@@ -35,23 +86,54 @@ impl Stream for EventFetcher {
         let mut all_logs: Vec<web3::types::Log> = vec![];
 
         for event in self.abi.iter() {
-            println!("{:?}", event);
+            let sig = event.signature();
+            let from_block: u64 = match self.db.get_last_logged_block(sig) {
+                Some(n) => n,
+                None => 0,
+            };
+
+            let filter = FilterBuilder::default()
+                .address(vec![self.address])
+                .from_block(BlockNumber::Number(from_block))
+                .topic_filter(TopicFilter {
+                    topic0: Topic::This(event.signature()),
+                    topic1: Topic::Any,
+                    topic2: Topic::Any,
+                    topic3: Topic::Any,
+                })
+                .build();
+
+            match self.web3.eth().logs(filter).wait().map_err(|e| e) {
+                Ok(v) => {
+                    let events = self.filter_events(event, v);
+                    if let Some(last_event) = events.last() {
+                        if let Some(block_num) = last_event.block_number {
+                            self.db.set_last_logged_block(sig, block_num.low_u64());
+                        };
+                    };
+                    all_logs.extend_from_slice(&events);
+
+                }
+                Err(e) => {
+                    println!("{}", e);
+                }
+            };
         }
         Ok(Async::Ready(Some(all_logs)))
     }
 }
 
-pub struct EventWatcher {
-    stream: EventFetcher,
+pub struct EventWatcher<T> where T: EventDb, {
+    stream: EventFetcher<T>,
     listeners: Vec<Box<dyn Fn(&Log) -> () + Send>>,
     _eloop: transports::EventLoopHandle,
 }
 
-impl EventWatcher {
-    pub fn new(url: &str, address: Address, abi: Vec<Event>) -> EventWatcher {
+impl<T> EventWatcher<T> where T: EventDb, {
+    pub fn new(url: &str, address: Address, abi: Vec<Event>, db: T) -> EventWatcher<T> {
         let (eloop, transport) = web3::transports::Http::new(url).unwrap();
         let web3 = web3::Web3::new(transport);
-        let stream = EventFetcher::new(web3, address, abi);
+        let stream = EventFetcher::new(web3, address, abi, db);
 
         EventWatcher {
             _eloop: eloop,
@@ -59,9 +141,13 @@ impl EventWatcher {
             listeners: vec![],
         }
     }
+
+    pub fn subscribe(&mut self, listener: Box<dyn Fn(&Log) -> () + Send>) {
+        self.listeners.push(listener);
+    }
 }
 
-impl Future for EventWatcher {
+impl<T> Future for EventWatcher<T> where T: EventDb, {
     type Item = ();
     type Error = ();
 
@@ -71,10 +157,17 @@ impl Future for EventWatcher {
                 Some(value) => value,
                 None => continue,
             };
+
+            for log in logs.iter() {
+                for listener in self.listeners.iter() {
+                    listener(log);
+                }
+            }
         }
     }
 }
 
+// Main
 fn main() {
     println!("Sample Watcher started");
 
@@ -107,6 +200,15 @@ fn main() {
         anonymous: false,
     }];
 
-    let mut watcher = EventWatcher::new("http://localhost:9545", address, abi);
-    tokio::run(watcher)
+    let db = DefaultEventDb::new();
+    let mut watcher = EventWatcher::new("http://localhost:9545", address, abi, db);
+
+    watcher.subscribe(Box::new(|log| {
+        println!("{:?}", log);
+    }));
+
+    tokio::run(future::lazy(|| {
+        tokio::spawn(watcher);
+        Ok(())
+    }));
 }
